2edf2d4fdb76101e7053c46cd3c4eb841d0a0d0a
camilo <camilo171512@gmail.com>
2026-09-18 10:39:35 -0500

    feat: :zap: empezando

---
+++ lib/presentation/widgets/app_drawer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          const DrawerHeader(child: Text("our presonal information"))
+        ],
+      ),
+    );
+  }
+}
