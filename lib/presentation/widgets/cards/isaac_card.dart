07a1b24acb2594b3c64678423563c3754609d7dc
Isaac <isaacdavidgarciavesga@gmail.com>
2026-09-18 18:08:42 -0500

    Profile

--- lib/presentation/widgets/cards/isaac_card.dart
+++ lib/presentation/widgets/cards/isaac_card.dart
@@ -24,11 +24,8 @@ class IsaacCard extends StatelessWidget {
                   children: [
                     CircleAvatar(
                       radius: 42,
-                      backgroundColor: theme.colorScheme.primary,
-                      child: Icon(
-                        Icons.person,
-                        color: theme.colorScheme.onPrimary,
-                        size: 42,
+                      backgroundImage: NetworkImage(
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqozC-IPacC5g-9EVSHnnnqWWcoLCLPHcHChTEQwD3UQ&s=10',
                       ),
                     ),
                     const SizedBox(width: 20),
